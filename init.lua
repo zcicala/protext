1cb196239e0bc73200bebc730a8b012959304d5d
Zac Cicala <zac.cicala@liveramp.com>
2020-12-13 10:02:46 -0800

    Add recent context shifting

--- init.lua
+++ init.lua
@@ -10,13 +10,16 @@ protextMenu = hs.menubar.new()
 protextMenu:setTitle("Protext")
 
 
+MAX_RECENT=10
 
 stateFile = "~/.hammerspoon/protextState.json"
 state = {
-	previousContext = "default",
+	currentContextPosition = 1,
+	recentSize = 1,
 	currentContext = "default",
 	contextData = {},
-	urlToContext = {}
+	urlToContext = {},
+	recentContexts = {"default"},
 }
 
 function Init(  )
@@ -94,25 +97,74 @@ function writeState(file)
 	hs.json.write(state, file, true, true)
 end
 
-
 function addToCurrentContext(url)
 	currentContext = state.currentContext
  	
  	table.insert(state.contextData[currentContext], url)
  	state.urlToContext[url] = currentContext
- 	writeState(stateFile)
  	
- 	hs.notify.show("Add to context",currentContext, url)
+	hs.notify.show("Add to context",currentContext, url)
+	addRecentContext(currentContext) 
+ 	writeState(stateFile)
 	--dump(state)
+end
+
+function shiftRecentContextPosition(diff)
+	state.currentContextPosition = state.currentContextPosition + diff
+	if state.currentContextPosition < 1 then
+		state.currentContextPosition = 1
+	end
+
+	if state.currentContextPosition > state.recentSize then
+		state.currentContextPosition = state.recentSize
+	end
+
+	switchContext(state.recentContexts[state.currentContextPosition])
+end
+
+function addRecentContext(context)
+	-- Is this context already the most recent item?
+	if state.recentContexts[state.recentSize]  == context then
+		return;
+	end
+
+	-- This is basically an LRU Cache, which I don't feel like implementing
+	-- Remove this context if its already in the list
+	indexToRemove = -1
+	for index, val in pairs(state.recentContexts) do
+		if val == context then
+			print(index, val)
+			indexToRemove = index
+			break
+		end
+	end
+
+	if indexToRemove > -1 then
+		tables.remove(state.recentContexts, indexToRemove)
+		state.recentSize = state.recentSize -1
+	end
+	
+	table.insert(state.recentContexts, context)
+	state.recentSize = state.recentSize + 1
+
+	-- if the table is too big then lets remove the first element
+	if state.recentSize > MAX_RECENT then
+		table.remove(state.recentContexts, 1)
+	end
+
+	--Position of currentContext has shifted
+	currentContextPosition = state.recentSize
+	updateUI()
 end
 
 function updateUI()
 	--Update menubar
 	menudata = {}
-	for key,value in pairs(state.contextData) do
+	--for key, value in pairs(state.contextData) do
+	for index, key in pairs(state.recentContexts) do
 		table.insert(menudata, {title = key, menu = {
-														{title ="Open", fn = function() openUrlsForContext(key) end}, 
-														{title ="Switch", fn = function() switchContext(key) end}
+				{title ="Open", fn = function() openUrlsForContext(key) end}, 
+				{title ="Switch", fn = function() switchContext(key) end}
 		}})	
 	end	
 	protextMenu:setMenu(menudata)
@@ -123,7 +175,6 @@ function switchContext(newContext)
 		state.contextData[newContext] = {}
 		updateUI()
 	end
-
 
 	state.previousContext = state.currentContext
 	state.currentContext = newContext
@@ -149,7 +200,11 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, ";", function()
 end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "[", function()
-	switchContext(state.previousContext)	 
+	shiftRecentContextPosition(-1) 
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "]", function()
+	shiftRecentContextPosition(1) 
 end)
 
 -- Extract title to new context
@@ -187,7 +242,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "/", function()
   else
   	print(currentAppName)
   	dump(currentApp)
-  	--dump(hs.axuielement.windowElement(currentApp:visibleWindows()[1]):allAttributeValues())
+  	dump(hs.axuielement.windowElement(currentApp:visibleWindows()[1]):allAttributeValues())
   end
 end)
 
